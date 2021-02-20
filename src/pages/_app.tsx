38d58cd0745e4f5dfaab6b335a9bdfa960f2801d
Bruno <brf147@gmail.com>
2021-02-20 18:34:08 -0400

    Feat: Add title and favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,16 @@ import { AppProps } from 'next/app';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core'
 import { theme } from '../styles/theme'
+import Head from 'next/head'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
       <ThemeProvider theme={theme}>
         <CssBaseline />
+        <Head>
+          <title>Feira de Ministérios 2021</title>
+        </Head>
         <Component {...pageProps} />
       </ThemeProvider>
     </>
